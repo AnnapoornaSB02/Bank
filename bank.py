f70d1c816a80a8b8063b496bc8faa8121668aac9
AnnapoornaSB02 <anubkatti02@gmail.com>
2025-10-06 17:16:45 +0530

    save

--- bank.py
+++ bank.py
@@ -6,3 +6,11 @@ new_balance = initial_balance + deposit
 print(f"\nInitial Balance: ₹{initial_balance}")
 print(f"Deposit: ₹{deposit}")
 print(f"New Balance after deposit: ₹{new_balance}")
+
+withdraw = float(input("Enter amount to withdraw: ₹"))
+
+final_balance = balance_after_deposit - withdraw
+
+
+print(f"Withdraw: ₹{withdraw}")
+print(f"Final Balance: ₹{final_balance}")
